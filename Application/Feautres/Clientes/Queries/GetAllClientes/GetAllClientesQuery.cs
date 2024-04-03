1969f19d4e48c146ab1591091d2c016f43a61cbe
Douglas <fullarukad@hotmail.com>
2024-04-02 22:20:03 -0500

    segundo commit avance hasta el video 18 del tutorial

---
+++ Application/Feautres/Clientes/Queries/GetAllClientes/GetAllClientesQuery.cs
@@ -0,0 +1,65 @@
+﻿using Application.DTOs;
+using Application.Interfaces;
+using Application.Specifications;
+using Application.Wrappers;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Application.Feautres.Clientes.Queries.GetAllClientes
+{
+    public class GetAllClientesQuery : IRequest<PagedResponse<List<ClienteDto>>>
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public string? Nombre { get; set; }
+
+        public string? Apellido { get; set; }
+
+        public class GetAllClientesQueryHandler : IRequestHandler<GetAllClientesQuery, PagedResponse<List<ClienteDto>>>
+        {
+            private readonly IRepositoryAsync<Cliente> _repositoryAsync;
+            private readonly IMapper _mapper;
+            private readonly IDistributedCache _distrbutedCache;
+
+            public GetAllClientesQueryHandler(IRepositoryAsync<Cliente> repositoryAsync, IMapper mapper, IDistributedCache distrbutedCache)
+            {
+                _repositoryAsync = repositoryAsync;
+                _mapper = mapper;
+                _distrbutedCache = distrbutedCache;
+            }
+            public async Task<PagedResponse<List<ClienteDto>>> Handle(GetAllClientesQuery request, CancellationToken cancellationToken)
+            {
+                var cacheKey = $"listadoClientes_{request.PageSize}_{request.PageNumber}_{request.Apellido}";
+                string serializedListadoCLientes;
+                var listadoClientes = new List<Cliente>();
+                var redisListadoClientes = await _distrbutedCache.GetAsync(cacheKey);
+
+                if (redisListadoClientes != null)
+                {
+                    serializedListadoCLientes = Encoding.UTF8.GetString(redisListadoClientes);
+                    listadoClientes = JsonConvert.DeserializeObject<List<Cliente>>(serializedListadoCLientes);
+                }
+                else
+                {
+                    listadoClientes = await _repositoryAsync.ListAsync(new PagedClientesSpecifications(request.PageSize, request.PageNumber, request.Nombre, request.Apellido));
+                    serializedListadoCLientes = JsonConvert.SerializeObject(listadoClientes);
+                    redisListadoClientes = Encoding.UTF8.GetBytes(serializedListadoCLientes);
+
+                    var options = new DistributedCacheEntryOptions()
+                        .SetAbsoluteExpiration(DateTime.Now.AddMinutes(10))
+                        .SetSlidingExpiration(TimeSpan.FromMinutes(2));
+
+                    await _distrbutedCache.SetAsync(cacheKey, redisListadoClientes, options);
+                }                
+                var clientesDto = _mapper.Map<List<ClienteDto>>(listadoClientes);
+
+                return new PagedResponse<List<ClienteDto>>(clientesDto,request.PageNumber,request.PageSize);
+            }
+        }
+    }
+}
--- WebAPI/Controllers/v1/ClientesController.cs
+++ WebAPI/Controllers/v1/ClientesController.cs
@@ -1,6 +1,8 @@
 ﻿using Application.Feautres.Clientes.Commands.CreateClienteCommand;
 using Application.Feautres.Clientes.Commands.DeleteClienteCommand;
 using Application.Feautres.Clientes.Commands.UpdateClienteCommand;
+using Application.Feautres.Clientes.Queries.GetAllClientes;
+using Application.Feautres.Clientes.Queries.GetClienteById;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers.v1
@@ -8,6 +10,20 @@ namespace WebAPI.Controllers.v1
     [ApiVersion("1.0")]
     public class ClientesController : BaseApiController
     {
+        //GET: api/<controller>/5
+        [HttpGet()]
+        public async Task<IActionResult> Get([FromQuery] GetAllClientesParameters filter)
+        {
+            return Ok(await Mediator.Send(new GetAllClientesQuery { PageNumber = filter.PageNumber, PageSize = filter.PageSize, Nombre= filter.Nombre, Apellido= filter.Apellido}));
+        }
+
+        //GET: api/<controller>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok(await Mediator.Send(new GetClienteByIdQuery { Id = id }));
+        }
+
         //POST api/<controller>
         [HttpPost]
         public async Task<IActionResult> Post(CreateClienteCommand command)
@@ -19,7 +35,7 @@ namespace WebAPI.Controllers.v1
         [HttpPut("{id}")]
         public async Task<IActionResult> Put(int id, UpdateClienteCommand command)
         {
-            if(id!= command.Id)
+            if (id != command.Id)
             {
                 return BadRequest();
             }
@@ -29,8 +45,8 @@ namespace WebAPI.Controllers.v1
         //Put api/<controller>/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(int id)
-        {           
-            return Ok(await Mediator.Send(new DeleteClienteCommand { Id= id}));
+        {
+            return Ok(await Mediator.Send(new DeleteClienteCommand { Id = id }));
         }
     }
 }
