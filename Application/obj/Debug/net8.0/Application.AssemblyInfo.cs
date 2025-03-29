0e87965ee6df8355f55934f72c1edc81b0a0897a
douglas hernandez <fullarukad@hotmail.com>
2025-03-29 18:54:21 -0500

    ajustes iniciando
    retomando el proyecto

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4308693891cc9f3867d43e7476c7e3dccc08ce61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01e35c841c62626f5b9363b704ec67e445473905")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
